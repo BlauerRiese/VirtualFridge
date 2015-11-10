7e90194a1e6afaa5b77e398ae7fe771117b904d2
MThuro <mareike.thurau@t-online.de>
2015-11-10 19:15:35 +0100

    Drawer Layout

--- app/src/main/java/bjoernbinzer/virtualfridge/FridgeActivity.java
+++ app/src/main/java/bjoernbinzer/virtualfridge/FridgeActivity.java
@@ -5,17 +5,29 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class FridgeActivity extends AppCompatActivity {
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String[] array = new String[]{};
+    private int counter;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,6 +56,8 @@ public class FridgeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fridge);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+        counter = 0;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +72,28 @@ public class FridgeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String category = ((TextView) findViewById(R.id.textViewVegetables)).getText().toString();
                 openListItem(view, category);
+            }
+        });
+
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerOpened(View drawerView){
+               super.onDrawerOpened(drawerView);
+               getSupportActionBar().setTitle("Einkaufsliste");
+               getSupportActionBar().show();
+            }
+            public void onDrawerClosed(View view){
+                super.onDrawerClosed(view);
+                getSupportActionBar().hide();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        ImageView add = (ImageView) findViewById(R.id.add_product);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText einkauf = (EditText) findViewById(R.id.EditText01);
+                addDrawerItems(einkauf);
             }
         });
 
@@ -150,6 +186,12 @@ public class FridgeActivity extends AppCompatActivity {
                 openListItem(view, category);
             }
         });
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplication(), "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
     public void openAddFridgeItem(View view) {
@@ -161,6 +203,21 @@ public class FridgeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ItemList.class);
         intent.putExtra(("Button"), category);
         startActivity(intent);
+    }
+    public void addDrawerItems(EditText einkauf){
+        String product = einkauf.getText().toString();
+        String[] arraylist = new String[counter+1];
+        if (counter != 0) {
+            for (int i = 0; i < array.length; i++) {
+                arraylist[i] = array[i];
+            }
+        }
+        arraylist[counter] = product;
+        array = arraylist;
+        counter += 1;
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, array);
+        mDrawerList.setAdapter(mAdapter);
+        einkauf.setText("");
     }
 
 }

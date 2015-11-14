0304001a51379e6927f7809382f5d837ec28688a
zarfty <benz.tamara@gmail.com>
2015-11-14 16:01:27 +0100

    Navigation Drawer Activity (alte FridgeActivity entfernt)

--- app/src/main/java/bjoernbinzer/virtualfridge/MainActivity.java
+++ app/src/main/java/bjoernbinzer/virtualfridge/MainActivity.java
@@ -1,63 +1,42 @@
 package bjoernbinzer.virtualfridge;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class FridgeActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private int counter;
     private ListView mDrawerList;
     private ArrayAdapter<String> mAdapter;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private String[] array = new String[]{};
-    private int counter;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fridge, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fridge);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().hide();
-        counter = 0;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -66,34 +45,22 @@ public class FridgeActivity extends AppCompatActivity {
                 openAddFridgeItem(view);
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         LinearLayout vegetableButton = (LinearLayout)findViewById(R.id.linearLayoutVegetables);
         vegetableButton.setOnClickListener(new LinearLayout.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String category = ((TextView) findViewById(R.id.textViewVegetables)).getText().toString();
                 openListItem(view, category);
-            }
-        });
-
-        mDrawerList = (ListView) findViewById(R.id.navList);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close){
-            public void onDrawerOpened(View drawerView){
-               super.onDrawerOpened(drawerView);
-               getSupportActionBar().setTitle("Einkaufsliste");
-               getSupportActionBar().show();
-            }
-            public void onDrawerClosed(View view){
-                super.onDrawerClosed(view);
-                getSupportActionBar().hide();
-            }
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        ImageView add = (ImageView) findViewById(R.id.add_product);
-        add.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                EditText einkauf = (EditText) findViewById(R.id.EditText01);
-                addDrawerItems(einkauf);
             }
         });
 
@@ -186,14 +153,77 @@ public class FridgeActivity extends AppCompatActivity {
                 openListItem(view, category);
             }
         });
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplication(), "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerOpened(View drawerView){
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Einkaufsliste");
+                getSupportActionBar().show();
+            }
+            public void onDrawerClosed(View view){
+                super.onDrawerClosed(view);
+                getSupportActionBar().hide();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        ImageView add = (ImageView) findViewById(R.id.add_product);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText einkauf = (EditText) findViewById(R.id.EditText01);
+                addDrawerItems(einkauf);
             }
         });
-
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     public void openAddFridgeItem(View view) {
         Intent intent = new Intent(this, AddFridgeItem.class);
         startActivity(intent);
@@ -204,6 +234,7 @@ public class FridgeActivity extends AppCompatActivity {
         intent.putExtra(("Button"), category);
         startActivity(intent);
     }
+
     public void addDrawerItems(EditText einkauf){
         String product = einkauf.getText().toString();
         String[] arraylist = new String[counter+1];
@@ -219,7 +250,4 @@ public class FridgeActivity extends AppCompatActivity {
         mDrawerList.setAdapter(mAdapter);
         einkauf.setText("");
     }
-
 }
-
-

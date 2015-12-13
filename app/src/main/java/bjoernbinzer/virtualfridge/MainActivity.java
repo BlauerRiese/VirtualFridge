a990f8cae0e2631dcd6ffa0ac90abe8deb17c045
zarfty <benz.tamara@gmail.com>
2015-12-14 00:55:55 +0100

    Benachrichtigung über Produkte die bald ablaufen bei App-Start

--- app/src/main/java/bjoernbinzer/virtualfridge/MainActivity.java
+++ app/src/main/java/bjoernbinzer/virtualfridge/MainActivity.java
@@ -1,11 +1,19 @@
 package bjoernbinzer.virtualfridge;
 
+import android.app.AlarmManager;
 import android.app.DialogFragment;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -25,7 +33,10 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, DeleteDialog.NoticeDialogListener{
@@ -37,6 +48,10 @@ public class MainActivity extends AppCompatActivity
     private DrawerLayout mDrawerLayout;
     private ArrayList<ShoppingListItem> array = new ArrayList<ShoppingListItem>();
     private ArrayList<ShoppingListItem> arrayNew = new ArrayList<ShoppingListItem>();
+    NotificationManager notificationManager;
+    PendingIntent pendingIntent;
+    final ArrayList<FridgeItem> productList = new ArrayList<FridgeItem>();
+    ArrayList<String> productNameList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +62,9 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().hide();
 
         FridgeDB.createFridgeDB(this);
+
+        handleCommand();
+        createNotification(productNameList);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -387,10 +405,104 @@ public class MainActivity extends AppCompatActivity
     public void onDialogNegativeClick(DialogFragment dialog) {
 
     }
+
     public void deleteItem(int item){
         mAdapter.remove(array.get(item));
         mAdapter.notifyDataSetChanged();
     }
 
+    public void handleCommand() {
+        //Get date of today, tomorrow and after tomorrow
+        Calendar today = Calendar.getInstance();
+        int day = today.get(Calendar.DATE);
+        int month = today.get(Calendar.MONTH);
+        month++;
+        int year = today.get(Calendar.YEAR);
+        String date = String.valueOf(day) + "." + String.valueOf(month) + "." + String.valueOf(year);
 
+        today.add(Calendar.DATE, 1);
+        int day2 = today.get(Calendar.DATE);
+        int month2 = today.get(Calendar.MONTH);
+        month2++;
+        int year2 = today.get(Calendar.YEAR);
+        String date2 = String.valueOf(day2) + "." + String.valueOf(month2) + "." + String.valueOf(year2);
+
+        today.add(Calendar.DATE, 1);
+        int day3 = today.get(Calendar.DATE);
+        int month3 = today.get(Calendar.MONTH);
+        month3++;
+        int year3 = today.get(Calendar.YEAR);
+        String date3 = String.valueOf(day3) + "." + String.valueOf(month3) + "." + String.valueOf(year3);
+
+        //Read from DB which products are due within the next 3 days
+        Cursor cursor = FridgeDB.getEntryByDate(date, date2, date3);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        String item;
+
+        //Read DB results
+        if (cursor.moveToFirst()) {
+            do {
+                String id = cursor.getString(0);
+                String name = cursor.getString(1);
+                Date durability = new Date();
+                try {
+                    durability = sdf.parse(cursor.getString(2));
+                } catch(Exception e) {};
+                double quantity = Double.parseDouble(cursor.getString(4));
+                String uom = cursor.getString(5);
+                double price = Double.parseDouble(cursor.getString(3));
+                String category = cursor.getString(6);
+                FridgeItem product = new FridgeItem(id, name, durability, quantity, uom, price, category);
+                productList.add(product);
+                if(uom.equals("Stück")){
+                    item = quantity + " " +name;
+                }else{
+                    item = quantity + " " + uom + " " +name;
+                }
+                productNameList.add(item);
+            } while (cursor.moveToNext());
+        }
+    }
+
+    //Create notification to notify user via the homescreen about products that need to be consumed soon
+    public void createNotification(ArrayList<String> productNameList) {
+        //Set notification icon, title and text
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_fridge)
+                .setContentTitle("Zeit zu kochen!")
+                .setContentText("Einige Produkte laufen demnächst ab.");
+
+        //Set long text of notification if user expands notification text
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle("Zeit zu kochen!");
+        inboxStyle.addLine("Einige Produkte laufen demnächst ab:");
+
+        //List products that are due soon in notification
+        String[] products = new String[productNameList.size()];
+        for (int i = 0; i < productNameList.size(); i++) {
+            inboxStyle.addLine(productNameList.get(i));
+        }
+
+        //Apply all settings to the notification
+        builder.setStyle(inboxStyle);
+
+        //Open first screen of the application if user clicks on notification
+        Intent resultIntent = new Intent(this, SplashScreen.class);
+
+        //Create TaskStackBuilder to handle back navigtion of notification
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+
+        //Update current pending intent
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+
+        //Get notification service of the system
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Build the notification and notify user on home screen
+        notificationManager.notify(0, builder.build());
+    }
 }
